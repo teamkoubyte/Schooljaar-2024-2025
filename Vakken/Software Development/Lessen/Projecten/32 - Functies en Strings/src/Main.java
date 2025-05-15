ba4c41772ccd91e5ee3e6dd0f25a33faf6f7987c
Koubaa <mohamed.koubaa@bazandpoort.be>
2025-05-15 11:27:53 +0200

    Alle schoolbestanden toevoegen

--- Vakken/Software Development/Lessen/Projecten/32 - Functies en Strings/src/Main.java
+++ Vakken/Software Development/Lessen/Projecten/32 - Functies en Strings/src/Main.java
@@ -1,30 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isCorrecteZin("Hallo, gaat alles goed."));
+        System.out.println(isCorrecteZin("Het is 4 uur."));
+        System.out.println(isCorrecteZin("het is 4 uur."));
+        System.out.println(isCorrecteZin("Het is vier uur"));
+        System.out.println(isCorrecteZin("hallo"));
+        System.out.println(begintMetHoofdletter("Hallo"));
+        System.out.println(begintMetHoofdletter("hallo"));
+        System.out.println(eindigtOpPuntVraagtekenOfUitroepteken("hallo!"));
+        System.out.println(eindigtOpPuntVraagtekenOfUitroepteken("Hallo"));
         System.out.println(geefAantalCijfersInZin("Het is 9u30."));
+        System.out.println(isAantalCijfersInZinEven("Het is 9u30."));
+
     }
 
-    public static boolean begintMetHoofdletter (String zin) {
+    public static boolean isCorrecteZin(String zin) {
+        return (begintMetHoofdletter(zin) && eindigtOpPuntVraagtekenOfUitroepteken(zin));
+    }
+
+    public static boolean begintMetHoofdletter(String zin) {
         boolean beginHoofdletter;
 
         beginHoofdletter = zin.charAt(0) >= 'A' && zin.charAt(0) <= 'Z';
-
-        return beginHoofdletter;
+        return (beginHoofdletter);
     }
 
-    public static boolean eindigtOpPuntVraagtekenOfUitroepteken (String zin) {
+    public static boolean eindigtOpPuntVraagtekenOfUitroepteken(String zin) {
         boolean eindePuntVraagtekenOfUitroepteken;
 
-        eindePuntVraagtekenOfUitroepteken = zin.endsWith(".") || zin.endsWith("?") || zin.endsWith("!");
-
-        return eindePuntVraagtekenOfUitroepteken;
+        eindePuntVraagtekenOfUitroepteken = zin.endsWith(".")
+                || zin.endsWith("?")
+                || zin.endsWith("!");
+        return (eindePuntVraagtekenOfUitroepteken);
     }
 
-    public static boolean isCorrecteZin (String zin) {
-       return (begintMetHoofdletter(zin) && eindigtOpPuntVraagtekenOfUitroepteken(zin));
-    }
-
-    public static int geefAantalCijfersInZin (String zin) {
+    public static int geefAantalCijfersInZin(String zin) {
         int aantalCijfers = 0;
         char karakter;
 
@@ -34,32 +43,29 @@ public class Main {
                 aantalCijfers++;
             }
         }
-        return aantalCijfers;
+        return (aantalCijfers);
     }
 
-    public static boolean isAantalCijfersInZinEven(String zin) {
-        int aantalCijfers = geefAantalCijfersInZin(zin);
-
-        return aantalCijfers % 2 == 0;
-    }
-
-    public static int geefAantalKlinkersInZin (String zin) {
+    public static int geefAantalKlinkersInZin(String zin) {
         int aantalKlinkers = 0;
         char karakter;
 
-        for (int teller = 0; teller < zin.length(); teller++) {
-            karakter = zin.charAt(teller);
-            if (karakter == 'a' || karakter == 'e' || karakter == 'i' || karakter == 'o' || karakter == 'u' || karakter == 'A' || karakter == 'E' || karakter == 'I' || karakter == 'O' || karakter == 'U') {
+        for (int teller = 0;teller < zin.length();teller++) {
+            karakter = zin.toUpperCase().charAt(teller);
+            if (karakter == 'A' || karakter == 'E' || karakter == 'I' ||
+                    karakter == 'O' || karakter == 'U') {
                 aantalKlinkers++;
             }
         }
-        return aantalKlinkers;
+        return(aantalKlinkers);
+    }
+
+    public static boolean isAantalCijfersInZinEven(String zin) {
+        return (geefAantalCijfersInZin(zin) % 2 == 0);
     }
 
     public static boolean bevatZinMeerKlinkersDanCijfers(String zin) {
-        int aantalKlinkers = geefAantalKlinkersInZin(zin);
-        int aantalCijfers = geefAantalCijfersInZin(zin);
-
-        return aantalKlinkers > aantalCijfers;
+        return(geefAantalKlinkersInZin(zin) > geefAantalCijfersInZin(zin));
     }
 }
+
